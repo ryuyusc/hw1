b3488ef3e61fc066c2e52d9cd09ce21400b24916
Youngbin Ryu <ryuy@usc.edu>
2023-01-19 06:45:48 +0000

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -13,11 +13,71 @@ the function below should be the only one in this file.
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
+/*
+*  this method takes the linked list and sets it to the tail
+*/
+void reachEnd(Node*& n) ;
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
+  //checks if the linked list is empty
+  if (in != nullptr) {
+    //gets the next value in 
+    int val = in->value;
+
+    //case for evens
+    if (val % 2 == 0) {
+      //case for empty array
+      if (evens == nullptr) {
+        evens = in;  //sets the head to current Node
+        in = in->next; //increments in so that current element's next is not modified
+        evens->next = nullptr;  //sets the next to NULL
+      }
+      //case for nonempty array
+      else {
+        //temp node* to come back to
+        Node* head = evens;
+        reachEnd(evens);
+
+        evens->next = in;  //sets the new Node
+        evens = evens->next;  //increments the element
+        in = in->next; //increments in so that the current element's next is not modified
+        evens->next = nullptr;  //sets the tail's next to NULL
+        evens = head;  //resets back to head
+      }
+    }
+    //case for odds
+    else {
+      //case for empty array
+      if (odds == nullptr) {
+        odds = in;  //sets the head to current Node
+        in = in->next; //increments in so that current element's next is not modified
+        odds->next = nullptr;  //sets the next to NULL
+      }
+      //case for nonempty array
+      else {
+        //temp node* to come back to
+        Node* head = odds;
+        reachEnd(odds);
+
+        odds->next = in;  //sets the new Node
+        odds = odds->next;  //increments the element
+        in = in->next; //increments in so that the current element's next is not modified
+        odds->next = nullptr;  //sets the tail's next to NULL
+        odds = head;  //resets back to head
+      }
+    }
+
+    split(in, odds, evens);  //recursive call
+  }
   /* Add code here */
 // WRITE YOUR CODE HERE
 }
 
 /* If you needed a helper function, write it here */
+void reachEnd(Node*& n) {
+  if (n->next != nullptr) {
+    n = n->next;
+    reachEnd(n);
+  }
+}
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,42 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
 
 int main(int argc, char* argv[])
 {
+  Node* list = nullptr;
+  Node* odds = nullptr;
+  Node* evens = nullptr;
 
+  Node seven(7, nullptr);
+  Node six(6, &seven);
+  Node five(5, &six);
+  Node four(4, &five);
+  Node three(3, &four);
+  Node two(2, &three);
+  Node one(1, &two);
+  Node zero(0, &one);
+ 
+  list = &zero;
+
+  split(list, odds, evens);
+
+  cout << "evens" << endl;
+  while (evens != nullptr) {
+    cout << evens->value << endl;
+    evens = evens->next;
+  }
+  cout << "odds" << endl;
+  while (odds != nullptr) {
+    cout << odds->value << endl;
+    odds = odds->next;
+  }
+
+  cout << "list" << endl;
+  cout << list << endl;
+  
 }
